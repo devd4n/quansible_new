1b7ba1060cf3e2a00f7d67fef45ad95b01208732
devd4n <devd4n@git.local>
2023-02-15 22:40:26 +0100

    basic fixing

--- quansible.sh
+++ quansible.sh
@@ -7,28 +7,28 @@ QUANSIBLE_VENV=""
 DOCKER_MODE=""
 
 function prepare_environment () {
-  sudo apt update
+  apt update
   # Install system requirements and apps for virtualenv
-  sudo apt install curl sudo python3-pip python3-venv -y
+  apt install curl sudo python3-pip python3-venv -y
   # https://www.codegrepper.com/code-examples/shell/python+headers+are+missing+in+%2Fusr%2Finclude%2Fpython3.6m+%26quot%3Byum%26quot%3B
   # https://stackoverflow.com/questions/31508612/pip-install-unable-to-find-ffi-h-even-though-it-recognizes-libffi
-  sudo apt install python-dev python3-dev libffi-dev -y
+  apt install python-dev python3-dev libffi-dev -y
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=998232
   #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   # ansible needs a UTF-8 locale
   
-  sudo snap install yq
+  snap install yq
   SCRIPT_DIR=$(pwd)
   USER_ADMIN="$(yq e '.quansible_user_admin' quansible/config.yaml)"
   ROOT_DIR="$(yq e '.quansible_root_dir' quansible/config.yaml)"
   DOCKER_MODE="$(yq e '.docker-mode' quansible/config.yaml)"
   ANSIBLE_VERSION="$(yq e '.quansible_ansible_version' quansible/config.yaml)"
   QUANSIBLE_VENV=$SCRIPT_DIR/venv
-  sudo useradd -m $USER_ADMIN --shell /bin/bash
-  sudo echo "$USER_ADMIN ALL=(ALL) NOPASSWD:ALL" >> sudo /etc/sudoers.d/$USER_ADMIN
+  useradd -m $USER_ADMIN --shell /bin/bash
+  echo "$USER_ADMIN ALL=(ALL) NOPASSWD:ALL" >> sudo /etc/sudoers.d/$USER_ADMIN
   
   # give full ownership to the ansible user
-  sudo chown -R $USER_ADMIN:$USER_ADMIN $SCRIPT_DIR
+  chown -R $USER_ADMIN:$USER_ADMIN $SCRIPT_DIR
   locale-gen en_GB.UTF-8
   #locale-gen en_GB
   update-locale LANG=en_GB.UTF-8
@@ -37,17 +37,17 @@ function prepare_environment () {
 
 # update quansible environment
 function prepare_ansible () {
-  su $USER_ADMIN
   # update user pip and initiate venv
-  python3 -m pip install --upgrade pip
-  python3 -m pip install virtualenv
-  python3 -m venv $QUANSIBLE_VENV
+  su -c "python3 -m pip install --upgrade pip ; && \
+    pip install virtualenv ; && \
+    venv $QUANSIBLE_VENV" -m "$USER_ADMIN" 
   
   # update venv, install ansible in venv
-  source $QUANSIBLE_VENV/bin/activate
-  python3 -m pip install --upgrade pip
-  python3 -m pip install wheel
-  python3 -m pip install ansible==$ANSIBLE_VERSION
+  su -c "venv $QUANSIBLE_VENV && \
+    source $QUANSIBLE_VENV/bin/activate ; && \
+    python3 -m pip install --upgrade ; pip && \
+    python3 -m pip install wheel ; && \
+    python3 -m pip install ansible==$ANSIBLE_VERSION"  -m "$USER_ADMIN" 
   return
 }
 
@@ -76,7 +76,7 @@ if [[ $1 == "setup-env" ]]
 then
   prepare_environment
   prepare_ansible
-  build_quansible
+  #build_quansible
   exit
 elif [[ $1 == "update" ]]
 then
